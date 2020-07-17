ce31bd82e58e21a2037e199a456a82aa78f507d9
iYoWei <swzyocowboy@icloud.com>
2020-07-17 17:19:51 +0800

    依赖: :arrow_up: 升级依赖

--- scripts/test.js
+++ scripts/test.js
@@ -1,16 +1,13 @@
 import execa from 'execa';
 
-// cross-env NODE_ENV=test npx nyc mocha --require @babel/register --no-opts --no-package 'src/**/?(*.)+(spec|test).[tj]s?(x)'
+// cross-env NODE_ENV=test npx nyc mocha --require @babel/register 'src/**/?(*.)+(spec|test).[tj]s?(x)'
 (async () => {
   try {
     const result = await execa('npx', [
       'nyc',
-      '--require',
-      '@babel/register',
       'mocha',
       '--require',
       '@babel/register',
-      '--no-opts',
       'src/**/?(*.)+(spec|test).[tj]s?(x)',
     ]);
 
